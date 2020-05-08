b82ac7f9e2867df382cc631fc908177a9ebf6b03
Elijah Bocz <boczelijah@gmail.com>
2020-05-07 22:41:40 -0400

    test bio page with md file

--- src/pages/bio.js
+++ src/pages/bio.js
@@ -1,17 +1,48 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import Layout from "../components/layout/layout";
+import { graphql } from "gatsby";
 
-function BioPage() {
+import Layout from "../components/layout/layout";
+import SEO from "../components/seo";
+
+const BioPage = props => {
+  const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter;
   return (
     <Layout>
       <Helmet>
         <meta charSet="utf-8" />
         <title>Bio</title>
       </Helmet>
-      <p>Bio</p>
+      <SEO title="Home" />
+      <h1>{data.title}</h1>
+      <p>{data.firstParagraph}</p>
+      <p>{data.secondParagraph}</p>
+      <p>{data.thirdParagraph}</p>
+      <p>{data.fourthParagraph}</p>
     </Layout>
   );
-}
+};
 
 export default BioPage;
+
+export const query = graphql`
+  query {
+    allFile(
+      filter: { sourceInstanceName: { eq: "content" }, name: { eq: "home" } }
+    ) {
+      edges {
+        node {
+          childMarkdownRemark {
+            frontmatter {
+              title
+              firstParagraph
+              secondParagraph
+              thirdParagraph
+              fourthParagraph
+            }
+          }
+        }
+      }
+    }
+  }
+`;
