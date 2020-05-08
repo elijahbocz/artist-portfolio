206a284d562528f5f1736296dce85f74e4031ff3
elijah <boczelijah@gmail.com>
2020-05-07 22:49:27 -0400

    add admin support for bio md

---
+++ src/pages/bio copy.js
@@ -0,0 +1,48 @@
+// import React from "react";
+// import { Helmet } from "react-helmet";
+// import { graphql } from "gatsby";
+
+// import Layout from "../components/layout/layout";
+// import SEO from "../components/seo";
+
+// const BioPage = props => {
+//   const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter;
+//   return (
+//     <Layout>
+//       <Helmet>
+//         <meta charSet="utf-8" />
+//         <title>Bio</title>
+//       </Helmet>
+//       <SEO title="Home" />
+//       <h1>{data.title}</h1>
+//       <p>{data.firstParagraph}</p>
+//       <p>{data.secondParagraph}</p>
+//       <p>{data.thirdParagraph}</p>
+//       <p>{data.fourthParagraph}</p>
+//     </Layout>
+//   );
+// };
+
+// export default BioPage;
+
+// export const query = graphql`
+//   query {
+//     allFile(
+//       filter: { sourceInstanceName: { eq: "content" }, name: { eq: "home" } }
+//     ) {
+//       edges {
+//         node {
+//           childMarkdownRemark {
+//             frontmatter {
+//               title
+//               firstParagraph
+//               secondParagraph
+//               thirdParagraph
+//               ourthParagraph
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
